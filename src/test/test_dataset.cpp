6dcc2bc8e933727e0b6d21e77295deb5a7d72ffe
liqinbin <godqinbin@gmail.com>
2020-10-19 21:17:05 +0800

    add googletest example

---
+++ src/test/test_dataset.cpp
@@ -0,0 +1,41 @@
+//
+// Created by liqinbin on 10/19/20.
+//
+#include "gtest/gtest.h"
+#include "FedTree/dataset.h"
+#include "FedTree/config.h"
+
+class DatasetTest : public ::testing::Test {
+public:
+    FLParams fl_param;
+    DataSet dataset;
+protected:
+    void SetUp() override {
+        dataset.load_from_file(DATASET_DIR "test_dataset.txt", param)
+    }
+};
+
+TEST_F(DatasetTest, load_from_file){
+    printf("### Dataset: %s, num_instances: %d, num_features: %d, get_cut_points finished. ###\n",
+        param.path.c_str(),
+        dataset.n_instances(),
+        dataset.n_features());
+    EXPECT_EQ(dataset.n_instances(), 1605);
+    EXPECT_EQ(dataset.n_features_, 119);
+    EXPECT_EQ(dataset.label[0], -1);
+    EXPECT_EQ(dataset.csr_val[1], 1);
+}
+
+//TEST(DatasetTest, load_dataset){
+//    DataSet dataset;
+//    FLParams param;
+//    dataset.load_from_file(DATASET_DIR "test_dataset.txt", param);
+//    printf("### Dataset: %s, num_instances: %d, num_features: %d, get_cut_points finished. ###\n",
+//    param.path.c_str(),
+//            dataset.n_instances(),
+//            dataset.n_features());
+//    EXPECT_EQ(dataset.n_instances(), 1605);
+//    EXPECT_EQ(dataset.n_features_, 119);
+//    EXPECT_EQ(dataset.label[0], -1);
+//    EXPECT_EQ(dataset.csr_val[1], 1);
+//}
---
+++ src/test/test_main.cpp
@@ -0,0 +1,17 @@
+//
+// Created by liqinbin on 10/19/20.
+//
+
+#include "FedTree/parser.h"
+#include "gtest/gtest.h"
+#ifdef _WIN32
+INITIALIZE_EASYLOGGINGPP
+#endif
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    el::Loggers::reconfigureAllLoggers(el::ConfigurationType::Format, "%datetime %level %fbase:%line : %msg");
+    el::Loggers::addFlag(el::LoggingFlag::ColoredTerminalOutput);
+    el::Loggers::addFlag(el::LoggingFlag::FixedTimeFormat);
+//    el::Loggers::reconfigureAllLoggers(el::ConfigurationType::Enabled, "false");
+    return RUN_ALL_TESTS();
+}
