f24d7ba7c3b7643c3a220281f9a0a4db57d2712a
Tianyuan <ftygary@gmail.com>
2020-10-19 18:14:30 +0800

    added DP noise functions

--- include/FedTree/DP/noises.h
+++ include/FedTree/DP/noises.h
@@ -7,9 +7,24 @@
 
 // Todo: Guassian noise and Laplacian noise
 
-template <typename T>
-void GuassianNoise(T& data, float variance){
+#include <random>
 
+template <typename T>
+void add_gaussian_noise(T& data, float variance) {
+    std::default_random_engine generator;
+    std::normal_distribution<double> distribution(0.0, variance);
+
+    double noise = distribution(generator);
+    *data += noise;
 }
 
+template <typename T>
+void add_laplasian_noise(T& data, float variance) {
+    // a r.v. following Laplace(0, b) is equivalent to the difference of 2 i.i.d Exp(1/b) r.v.
+    double b = sqrt(variance/2);
+    std::default_random_engine generator;
+    std::exponential_distribution<double> distribution(1/b);
+    double noise = distribution(generator) - distribution(generator);
+    *data += noise;
+}
 #endif //FEDTREE_NOISES_H
