b2a442b88fd9e810f9c1c7657088464e1eb5f0a6
yllekjjirpvul98 <kyungchingman@gmail.com>
2020-10-22 11:49:47 +0800

    include python

---
+++ include/FedTree/Encryption/HomomorphicEncrpytion.h
@@ -0,0 +1,15 @@
+//
+// Created by kellyyung on 19/10/2020.
+//
+
+#ifndef FEDERATEDLEARNINH_HOMOMORPHICENCRPYTION_H
+#define FEDERATEDLEARNINH_HOMOMORPHICENCRPYTION_H
+
+#include "Python.h"
+
+class HomomorphicEncrpytion {
+public:
+};
+
+
+#endif //FEDERATEDLEARNINH_HOMOMORPHICENCRPYTION_H
