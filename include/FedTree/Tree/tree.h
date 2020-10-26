fa7f37cb9c7dd96586b4e1eb5f26d0ce27bd6fa8
Tianyuan <ftygary@gmail.com>
2020-10-26 14:58:21 +0800

    [WIP] implement tree methods

--- include/FedTree/Tree/tree.h
+++ include/FedTree/Tree/tree.h
@@ -1,13 +1,74 @@
 //
 // Created by liqinbin on 10/13/20.
+// The tree structure is referring to the design of ThunderGBM: https://github.com/Xtra-Computing/thundergbm/blob/master/include/thundergbm/tree.h
 //
 
 #ifndef FEDTREE_TREE_H
 #define FEDTREE_TREE_H
 
-// Todo: the tree structure. Refer to ThunderGBM https://github.com/Xtra-Computing/thundergbm/blob/master/include/thundergbm/tree.h
-class Tree{
+#include "sstream"
+#include "syncarray.h"
+#include "GBDTparam.h"
 
+class Tree{
+public:
+    struct TreeNode {
+        int final_id;// node id after pruning, may not equal to node index
+        int lch_index;// index of left child
+        int rch_index;// index of right child
+        int parent_index;// index of parent node
+        float_type gain;// gain of splitting this node
+        float_type base_weight;
+        int split_feature_id;
+        float_type split_value;
+        unsigned char split_bid;
+        bool default_right;
+        bool is_leaf;
+        bool is_valid;// non-valid nodes are those that are "children" of leaf nodes
+        bool is_pruned;// pruned after pruning
+
+        GHPair sum_gh_pair;
+
+        friend std::ostream &operator<<(std::ostream &os,
+                                        const TreeNode &node);
+
+        HOST_DEVICE void calc_weight(float_type lambda) {
+            this->base_weight = -sum_gh_pair.g / (sum_gh_pair.h + lambda);
+        }
+
+        HOST_DEVICE bool splittable() const {
+            return !is_leaf && is_valid;
+        }
+
+    };
+
+    Tree() = default;
+
+    Tree(const Tree &tree) {
+        nodes.resize(tree.nodes.size());
+        nodes.copy_from(tree.nodes);
+    }
+
+    Tree &operator=(const Tree &tree) {
+        nodes.resize(tree.nodes.size());
+        nodes.copy_from(tree.nodes);
+        return *this;
+    }
+
+    void init2(const SyncArray<GHPair> &gradients, const GBDTParam &param);
+
+    string dump(int depth) const;
+
+    SyncArray<Tree::TreeNode> nodes;
+
+    void prune_self(float_type gamma);
+
+    private:
+    void preorder_traversal(int nid, int max_depth, int depth, string &s) const;
+
+    int try_prune_leaf(int nid, int np, float_type gamma, vector<int> &leaf_child_count);
+
+    void reorder_nid();
 };
 
 #endif //FEDTREE_TREE_H
