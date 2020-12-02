21594b093a7a9b6118da0bce718ba6c30f64a558
Tianyuan <ftygary@gmail.com>
2020-12-02 20:51:41 +0800

    [WIP] histcut

---
+++ src/FedTree/Tree/hist_cut.cpp
@@ -0,0 +1,7 @@
+// created by Tianyuan on 12/1/20
+
+#include "FedTree/Tree/hist_cut.h"
+
+void get_cut_points(DataSet data, float_type feature_min, float_type feature_max, int max_num_bins, int n_instances) {
+
+}
