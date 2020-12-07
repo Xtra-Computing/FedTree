cf8cf15912018832101da2db630c1ea6cd205971
Tianyuan <ftygary@gmail.com>
2020-12-07 14:06:28 +0800

    minor changes in including path

--- include/FedTree/dataset.h
+++ include/FedTree/dataset.h
@@ -2,12 +2,14 @@
 // Created by liqinbin on 10/13/20.
 //
 
+
+#ifndef FEDTREE_DATASET_H
+#define FEDTREE_DATASET_H
+
 #include "FL/FLparam.h"
 #include "common.h"
 #include "syncarray.h"
 
-#ifndef FEDTREE_DATASET_H
-#define FEDTREE_DATASET_H
 
 // Todo: datset structure (csr and csc). load from file. Refer to ThunderGBM dataset.h https://github.com/Xtra-Computing/thundergbm/blob/master/include/thundergbm/dataset.h
 
