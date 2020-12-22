a5c6ce8b321fbbd83478b2f3664397c39678d7bf
liqinbin <godqinbin@gmail.com>
2020-12-22 14:43:35 +0800

    update party&server structure

--- include/FedTree/FL/party.h
+++ include/FedTree/FL/party.h
@@ -9,41 +9,51 @@
 #include "FedTree/Tree/tree_builder.h"
 #include "FedTree/Encryption/HE.h"
 #include "FedTree/DP/noises.h"
+#include "FLparam.h"
+#include "server.h"
+#include "FedTree/booster.h"
 
 // Todo: the party structure
 class Party {
 public:
-    void init(int pid, const DataSet &dataSet) {
+    void init(int pid, DataSet &dataset, FLParam &param) {
         this->pid = pid;
         this->dataset = dataset;
+        booster.init(dataset, param.gbdt_param);
     };
 
     void homo_init() {
         std::tuple<AdditivelyHE::PaillierPublicKey, AdditivelyHE::PaillierPrivateKey> keyPairs = this->HE.generate_key_pairs();
         publicKey = std::get<0>(keyPairs);
         privateKey = std::get<1>(keyPairs);
-        fbuilder->encrypt_gradients(publicKey);
+        booster.fbuilder->encrypt_gradients(publicKey);
     };
 
     void overwrite_gradients(SyncArray<GHPair> gh){
-        fbuilder->set_gradients(gh);
+        booster.fbuilder->set_gradients(gh);
     };
 
     void send_gradients(Party &party){
-        party.overwrite_gradients(fbuilder->get_gradients());
+        party.overwrite_gradients(booster.fbuilder->get_gradients());
+    }
+
+    void send_last_trees(Server &server){
+        server.local_trees[pid].trees[0] = this->gbdt.trees.back();
     }
 
     int pid;
     AdditivelyHE::PaillierPublicKey publicKey;
     AdditivelyHE::PaillierPublicKey serverKey;
-    std::unique_ptr<TreeBuilder> fbuilder;
+//    std::unique_ptr<TreeBuilder> fbuilder;
 //    vector<SplitCandidate> split_candidates;
-
+    Booster booster;
+    GBDT gbdt;
 private:
     DataSet dataset;
     AdditivelyHE HE;
     AdditivelyHE::PaillierPrivateKey privateKey;
     DPnoises<double> DP;
+
 
 };
 
