7929d4cb1c49f01ba47e70de8ae62b23803b3780
QinbinLi <liqinbin1998@gmail.com>
2021-12-27 14:23:43 +0800

    update example

--- python/examples/classifier_example.py
+++ python/examples/classifier_example.py
@@ -7,8 +7,8 @@ from sklearn.metrics import accuracy_score
 
 if __name__ == '__main__':
     x, y = load_digits(return_X_y=True)
-    clf = FLClassifier()
+    clf = FLClassifier(n_trees=2)
     clf.fit(x, y)
     y_pred = clf.predict(x)
     accuracy = accuracy_score(y, y_pred)
-    print(accuracy)
+    print("accuracy:", accuracy)
