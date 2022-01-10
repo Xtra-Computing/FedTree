24fa7ffd57de6fe39bd4ecc3f828bb4cbcc59e8a
Qinbin Li <liqinbin1998@gmail.com>
2022-01-10 15:07:08 +0800

    fix label of test dataset

--- src/FedTree/main.cpp
+++ src/FedTree/main.cpp
@@ -105,6 +105,7 @@ int main(int argc, char** argv){
     if (use_global_test_set) {
         test_dataset.label_map = dataset.label_map;
         test_dataset.load_from_file(model_param.test_path, fl_param);
+        test_dataset.label = dataset.label;
     }
 
 //    if (ObjectiveFunction::need_group_label(param.gbdt_param.objective)) {
