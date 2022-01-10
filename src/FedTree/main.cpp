af6d347a3660ec2fa4fdcc6ae4dd4f51957ef55e
QinbinLi <liqinbin1998@gmail.com>
2022-01-10 14:18:49 +0800

    fix wrong prediction label

--- src/FedTree/main.cpp
+++ src/FedTree/main.cpp
@@ -102,8 +102,10 @@ int main(int argc, char** argv){
     }
 
     DataSet test_dataset;
-    if (use_global_test_set)
+    if (use_global_test_set) {
         test_dataset.load_from_file(model_param.test_path, fl_param);
+        test_dataset.label = dataset.label;
+    }
 
 //    if (ObjectiveFunction::need_group_label(param.gbdt_param.objective)) {
 //        group_label();
