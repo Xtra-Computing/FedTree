7143443eb193924e772613f3e68601ffa37864c6
QinbinLi <liqinbin1998@gmail.com>
2022-01-10 15:04:27 +0800

    update n_classes

--- src/FedTree/main.cpp
+++ src/FedTree/main.cpp
@@ -114,22 +114,23 @@ int main(int argc, char** argv){
 
     GBDTParam &param = fl_param.gbdt_param;
 
-//    if (param.objective.find("multi:") != std::string::npos || param.objective.find("binary:") != std::string::npos || param.metric == "error") {
+    //correct the number of classes
+    if (param.objective.find("multi:") != std::string::npos || param.objective.find("binary:") != std::string::npos || param.metric == "error") {
 //        for (int i = 0; i < n_parties; i++) {
 //            train_subsets[i].group_label();
 //            test_subsets[i].group_label();
 //        }
-//        int num_class = dataset.label.size();
-//        if (param.num_class != num_class) {
-//            LOG(INFO) << "updating number of classes from " << param.num_class << " to " << num_class;
-//            param.num_class = num_class;
-//        }
-//        if(param.num_class > 2)
-//            param.tree_per_rounds = param.num_class;
-//    }
-//    else if(param.objective.find("reg:") != std::string::npos){
-//        param.num_class = 1;
-//    }
+        int num_class = dataset.label.size();
+        if (param.num_class != num_class) {
+            LOG(INFO) << "updating number of classes from " << param.num_class << " to " << num_class;
+            param.num_class = num_class;
+        }
+        if(param.num_class > 2)
+            param.tree_per_rounds = param.num_class;
+    }
+    else if(param.objective.find("reg:") != std::string::npos){
+        param.num_class = 1;
+    }
 
     vector<Party> parties(n_parties);
     vector<int> n_instances_per_party(n_parties);
@@ -152,18 +153,18 @@ int main(int argc, char** argv){
 
 
     //correct the number of classes
-    if(param.objective.find("multi:") != std::string::npos || param.objective.find("binary:") != std::string::npos) {
-        int num_class = dataset.label.size();
-        if (param.num_class != num_class) {
-            LOG(INFO) << "updating number of classes from " << param.num_class << " to " << num_class;
-            param.num_class = num_class;
-        }
-        if(param.num_class > 2)
-            param.tree_per_rounds = param.num_class;
-    }
-    else if(param.objective.find("reg:") != std::string::npos){
-        param.num_class = 1;
-    }
+//    if(param.objective.find("multi:") != std::string::npos || param.objective.find("binary:") != std::string::npos) {
+//        int num_class = dataset.label.size();
+//        if (param.num_class != num_class) {
+//            LOG(INFO) << "updating number of classes from " << param.num_class << " to " << num_class;
+//            param.num_class = num_class;
+//        }
+//        if(param.num_class > 2)
+//            param.tree_per_rounds = param.num_class;
+//    }
+//    else if(param.objective.find("reg:") != std::string::npos){
+//        param.num_class = 1;
+//    }
 
     LOG(INFO) << "start training";
     FLtrainer trainer;
