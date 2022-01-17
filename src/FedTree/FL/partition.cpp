e54e452ad39b1920575b044f895b9fe054150337
QinbinLi <liqinbin1998@gmail.com>
2022-01-17 14:34:46 +0800

    improve partition

--- src/FedTree/FL/partition.cpp
+++ src/FedTree/FL/partition.cpp
@@ -41,22 +41,16 @@ void Partition::homo_partition(const DataSet &dataset, const int n_parties, cons
 //    std::map<int, vector<int>> batch_idxs;
 
     int stride = n / n_parties;
-    for (int i = 0; i < n_parties; i++) {
+    for (int i = 0; i < n_parties - 1; i++) {
         batch_idxs[i] = vector<int>(idxs.begin() + i * stride, idxs.begin() + (i + 1) * stride);
         thrust::sort(thrust::host, batch_idxs[i].begin(), batch_idxs[i].begin() + batch_idxs[i].size());
     }
     //LOG(INFO)<<"batch_idxs:"<<batch_idxs;
 
-    if (stride * n_parties < n) {
-        for (int i = stride * n_parties; i < n; i++) {
-            batch_idxs[n_parties - 1].push_back(idxs[i]);
-        }
-        thrust::sort(thrust::host, batch_idxs[n_parties - 1].begin(), batch_idxs[n_parties - 1].begin() + batch_idxs[n_parties - 1].size());
+    for (int i = stride * (n_parties - 1); i < n; i++) {
+        batch_idxs[n_parties - 1].push_back(idxs[i]);
     }
-
-//    for (int i = 0; i < n % n_parties; i++) {
-//        batch_idxs[i].push_back(idxs[n_parties * stride + i]);
-//    }
+    thrust::sort(thrust::host, batch_idxs[n_parties - 1].begin(), batch_idxs[n_parties - 1].begin() + batch_idxs[n_parties - 1].size());
 
     vector<int> part2party(n);
     for (int i = 0; i < n_parties; i++) {
